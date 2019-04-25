386b8e0cf3e2a1d8f35faf160864f48e1625ee51
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-04-25 13:07:49 -0500

    got some player physics in there...doesn't work quite how I want it but I'll be working on it

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let screenWidth = 800;
 let screenHeight = 600;
 let tileSize = 32;
+let gravityY = 500;
 
 var config = {
     type: Phaser.AUTO,
@@ -9,7 +10,7 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 500 },
+            gravity: { y: gravityY },
         debug: false
         }
     },
@@ -34,15 +35,59 @@ function preload() {
 }
 
 function create() {
+
     platforms = this.physics.add.staticGroup();
+    cursors = this.input.keyboard.createCursorKeys();
 
     this.add.image(400, 300, 'sky');
 
     ground = this.add.tileSprite(tileSize/2, screenHeight - (tileSize/2), 1600, 32, 'ground');
 
-    
+    platforms.add(ground);
     //platforms.create(tileSize, screenHeight - tileSize, 'ground');
+    player = this.physics.add.sprite(100, 400, 'megaMan');
+    player.setBounce(0);
+    player.setCollideWorldBounds(true);
+    this.anims.create({
+        key: 'run',
+        frames: this.anims.generateFrameNumbers('megaMan', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'idle',
+        frames: [{ key: 'megaMan', frame: 0 }],
+        frameRate: 20
+    });
+    player.body.setGravityY(gravityY);
+
+    this.physics.add.collider(player, platforms);
 }
 
 function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('run', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('run', true);
+    }
+    else {
+        player.setVelocityX(0);
+        player.anims.play('idle');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-300);
+    }
+
+    if (player.velocityY < 0) {
+        player.setGravityY(gravityY * 2);
+        console.log('fast drop');
+        console.log('player gravity:', player.gravityY);
+    }
+    else {
+        player.setGravityY(gravityY);
+    }
 }
